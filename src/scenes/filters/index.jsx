50375bc3d08bbc63f91663e69e993e687950f8b1
Anil <puttaanilkumar666@gmail.com>
2026-02-14 18:51:24 -0600

    Chore: Remove all unused imports and fix Filters component formatting

--- src/scenes/filters/index.jsx
+++ src/scenes/filters/index.jsx
@@ -1,12 +1,8 @@
-import { useContext, useEffect } from "react";
-import { filterContext } from "../../contexts/filterContext";
-import pokeApi from "../../services/pokeApi";
-import { pokeContext } from "../../contexts/pokeContext";
-import { loadingContext } from "./../../contexts/loadingContext";
+
 
 const Filters = () => {
   // This component is currently empty after removing unused code for build success.
   return null;
-}
+};
 
 export default Filters;
