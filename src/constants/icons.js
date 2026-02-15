bb245c822e55e7c72986a41d50e28c4c88f45de3
Anil <puttaanilkumar666@gmail.com>
2026-02-14 18:59:25 -0600

    Fix: Use static imports for type icons for Vercel compatibility

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,12 +1,42 @@
-const importAll = (requireContext) => {
-  const icons = {};
-  requireContext.keys().forEach((key) => {
-    const iconName = key.replace('./', '').replace('.png', '');
-    icons[iconName] = requireContext(key);
-  });
-  return icons;
-};
 
-const icons = importAll(require.context('../assets/img/icons/types', false, /\.png$/));
+import bug from '../assets/img/icons/types/bug.png';
+import dark from '../assets/img/icons/types/dark.png';
+import dragon from '../assets/img/icons/types/dragon.png';
+import electric from '../assets/img/icons/types/electric.png';
+import fairy from '../assets/img/icons/types/fairy.png';
+import fighting from '../assets/img/icons/types/fighting.png';
+import fire from '../assets/img/icons/types/fire.png';
+import flying from '../assets/img/icons/types/flying.png';
+import ghost from '../assets/img/icons/types/ghost.png';
+import grass from '../assets/img/icons/types/grass.png';
+import ground from '../assets/img/icons/types/ground.png';
+import ice from '../assets/img/icons/types/ice.png';
+import normal from '../assets/img/icons/types/normal.png';
+import poison from '../assets/img/icons/types/poison.png';
+import psychic from '../assets/img/icons/types/psychic.png';
+import rock from '../assets/img/icons/types/rock.png';
+import steel from '../assets/img/icons/types/steel.png';
+import water from '../assets/img/icons/types/water.png';
+
+const icons = {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water,
+};
 
 export default icons;
